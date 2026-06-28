52b86fa2c2771bf1df679f59181dd0f74b770649
lillcreeperdude <windy.sleeper@gmail.com>
2026-06-27 23:06:11 -0700

    started the parser

---
+++ parser.js
@@ -0,0 +1,30 @@
+/**
+ * parser.js -- minecraft addon parser
+ * ————————————————————————————————————————
+ * unzips an mcaddon file and extracting assets, recipies
+ * and names from the behavior pack and resourse pack
+ * 
+ * Depens on: JSZip
+ */
+
+
+// #region get file
+//passes the click from button to file input
+document.getElementById("my-button").addEventListener("click",()=>{
+    document.getElementById("mcaddon-input").click();
+});
+
+//gets the raw data from the mcaddon
+const input = document.getElementById("mcaddon-input")
+
+
+//listins for when user is done picking file
+input.addEventListener("change",(event)=>{
+    //sets file to the selected file
+    const file = event.target.files[0];
+    console.log(file)
+});
+// #endregion
+// #region unzip file
+
+// #endregion
